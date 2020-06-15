42f0dff4d268f61c8f57c02ef67f365980b0ba8b
Mani <mani@News-MacBook-Pro.local>
2020-06-15 16:05:33 +0530

    Django Admin panel modified

---
+++ listings/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+#from .models import Listing
+
+
+
+def index(request):
+    #listings = Listing.objects.all()
+    return render(request, 'listings/listings.html')
+
+def listing(request):
+    return render(request, 'listings/listing.html')
+
+def search(request):
+    return render(request, 'listings/search.html')
--- realtor/settings.py
+++ realtor/settings.py
@@ -20,6 +20,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'rltrs.apps.RltrsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,8 +66,11 @@ WSGI_APPLICATION = 'realtor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'manidb',
+        'USER': 'postgres',
+        'PASSWORD': 'Rama',
+        'HOST': 'localhost'
     }
 }
 
@@ -111,3 +116,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realtor/static')
 ]
+
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
